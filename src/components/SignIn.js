b3e9827e97d016e13b77299713746d2675cd51ce
takagi <takagi09245@gmail.com>
2022-10-28 01:04:59 +0900

    create config

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -34,7 +34,7 @@ const theme = createTheme();
 export default function SignIn({ setName }) {
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState("");
-  console.log(string);
+  const [isConposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === "";
@@ -81,10 +81,18 @@ export default function SignIn({ setName }) {
               autoFocus
               onChange={(e) => setString(e.target.value)}
               onKeyDown={(e) => {
+                if (isConposed) return;
+
                 if (e.key === "Enter") {
                   setName(e.target.value);
                   e.preventDefault();
                 }
+              }}
+              onCompositionStart={() => {
+                setIsComposed(true);
+              }}
+              onCompositionEnd={() => {
+                setIsComposed(false);
               }}
             />
             <Button
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css';
-import App from './components/App';
+import "./index.css";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
