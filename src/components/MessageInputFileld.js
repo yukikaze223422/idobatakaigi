8859888623278e1a42ba4885851a75d46f0e50bf
takagi <takagi09245@gmail.com>
2022-10-29 23:45:17 +0900

    apply grid to input component

--- src/components/MessageInputFileld.js
+++ src/components/MessageInputFileld.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
 
 const MessageInputField = () => {
-  return <div style={{ gridRow: 2 }}>MessageInputField</div>;
+  return (
+    <div style={{ gridRow: 2, margin: "26px" }}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default MessageInputField;
