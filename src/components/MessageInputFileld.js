adda9ccb05ae0bbea71b6c22a64290c10f9fcaf2
takagi <takagi09245@gmail.com>
2022-10-30 00:12:15 +0900

    generate avatar

--- src/components/MessageInputFileld.js
+++ src/components/MessageInputFileld.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
+import { gravatarPath } from "../gravatar";
 
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
+  const avatarPath = gravatarPath("takagi09245@gmail.com");
   return (
     <div style={{ gridRow: 2, margin: "26px" }}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar />
+          <Avatar src={avatarPath} />
         </Grid>
         <Grid xs={10}>入力</Grid>
         <Grid xs={1}>ボタン</Grid>
