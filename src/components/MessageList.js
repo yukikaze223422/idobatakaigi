a4ee6eb1f8438153272cd62ba4c08071456f6622
takagi <takagi09245@gmail.com>
2022-11-06 20:02:33 +0900

    fetch messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  ref,
+  onValue,
+  query,
+  orderByKey,
+  limitToLast,
+} from "firebase/database";
+import { db } from "../firebase";
 
 const MessageList = () => {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const dbref = ref(db, "messages");
+    onValue(query(dbref, orderByKey(), limitToLast(3)), (snapshot) => {
+      const messages = snapshot.val();
+      const entries = Object.entries(messages);
+      if (messages === null) return;
+      const newMessages = entries.map((entry) => {
+        const [key, nameAndText] = entry;
+        return { key, ...nameAndText };
+      });
+      setMessages(newMessages);
+    });
+  }, []);
   return <div style={{ gridRow: 1 }}>MessageList</div>;
 };
 
